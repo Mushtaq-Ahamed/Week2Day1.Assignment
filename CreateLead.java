e647e8d5b3e8739e0f4864f52b4d3eeaaf6aa315
Mushtaq-Ahamed <60561585+Mushtaq-Ahamed@users.noreply.github.com>
2022-02-26 21:27:55 +0530

    Add files via upload

---
+++ CreateLead.java
@@ -0,0 +1,55 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class CreateLead {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().window().maximize();
+		WebElement elementUsername = driver.findElement(By.id("username"));
+		elementUsername.sendKeys("Demosalesmanager");
+		WebElement elementPassword = driver.findElement(By.id("password"));
+		elementPassword.sendKeys("crmsfa");
+		WebElement elementLoginButton = driver.findElement(By.className("decorativeSubmit"));
+		elementLoginButton.click();
+		WebElement elementCRMSFA = driver.findElement(By.linkText("CRM/SFA"));
+		elementCRMSFA.click();
+		WebElement elementLeads = driver.findElement(By.linkText("Leads"));
+		elementLeads.click();
+		WebElement elementCreateLead = driver.findElement(By.linkText("Create Lead"));
+		elementCreateLead.click();
+		WebElement elementCompanyName = driver.findElement(By.id("createLeadForm_companyName"));
+		elementCompanyName.sendKeys("CSC India Pvt Ltd");
+		WebElement elementFirstName = driver.findElement(By.id("createLeadForm_firstName"));
+		elementFirstName.sendKeys("Mushtaq");
+		WebElement elementLastName = driver.findElement(By.id("createLeadForm_lastName"));
+		elementLastName.sendKeys("Ahamed");
+		
+		
+		WebElement elementFirstNamelocal = driver.findElement(By.id("createLeadForm_firstNameLocal"));
+		elementFirstNamelocal.sendKeys("XYZ");
+		WebElement elementDepartmentName = driver.findElement(By.id("createLeadForm_departmentName"));
+		elementDepartmentName.sendKeys("HealthCare");
+		WebElement elementDescription = driver.findElement(By.id("createLeadForm_description"));
+		elementDescription.sendKeys("Service Based Company");
+		WebElement elementPrimaryEmail = driver.findElement(By.id("createLeadForm_primaryEmail"));
+		elementPrimaryEmail.sendKeys("xyz@gmail.com");
+		
+		WebElement elementStateProvince = driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+		Select dd = new Select(elementStateProvince);
+		dd.selectByVisibleText("NewYork");
+		
+		WebElement elementCreateLeadButton = driver.findElement(By.className("smallSubmit"));
+		elementCreateLeadButton.click();
+		
+		System.out.println("Title of Resulting Page is :" + driver.getTitle());
+	}
+}
---
+++ EditLead.java
@@ -0,0 +1,67 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class EditLead {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().window().maximize();
+		WebElement elementUsername = driver.findElement(By.id("username"));
+		elementUsername.sendKeys("Demosalesmanager");
+		WebElement elementPassword = driver.findElement(By.id("password"));
+		elementPassword.sendKeys("crmsfa");
+		WebElement elementLoginButton = driver.findElement(By.className("decorativeSubmit"));
+		elementLoginButton.click();
+		WebElement elementCRMSFA = driver.findElement(By.linkText("CRM/SFA"));
+		elementCRMSFA.click();
+		WebElement elementLeads = driver.findElement(By.linkText("Leads"));
+		elementLeads.click();
+		WebElement elementCreateLead = driver.findElement(By.linkText("Create Lead"));
+		elementCreateLead.click();
+		WebElement elementCompanyName = driver.findElement(By.id("createLeadForm_companyName"));
+		elementCompanyName.sendKeys("CSC India Pvt Ltd");
+		WebElement elementFirstName = driver.findElement(By.id("createLeadForm_firstName"));
+		elementFirstName.sendKeys("Mushtaq");
+		WebElement elementLastName = driver.findElement(By.id("createLeadForm_lastName"));
+		elementLastName.sendKeys("Ahamed");
+		
+		
+		WebElement elementFirstNamelocal = driver.findElement(By.id("createLeadForm_firstNameLocal"));
+		elementFirstNamelocal.sendKeys("XYZ");
+		WebElement elementDepartmentName = driver.findElement(By.id("createLeadForm_departmentName"));
+		elementDepartmentName.sendKeys("HealthCare");
+		WebElement elementDescription = driver.findElement(By.id("createLeadForm_description"));
+		elementDescription.sendKeys("Service Based Company");
+		WebElement elementPrimaryEmail = driver.findElement(By.id("createLeadForm_primaryEmail"));
+		elementPrimaryEmail.sendKeys("xyz@gmail.com");
+		WebElement elementStateProvince = driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+		Select dd = new Select(elementStateProvince);
+		dd.selectByVisibleText("New York");
+		WebElement elementCreateLeadButton = driver.findElement(By.className("smallSubmit"));
+		elementCreateLeadButton.click();
+		
+		WebElement elementEditButton = driver.findElement(By.className("subMenuButton"));
+		elementEditButton.click();
+		
+		WebElement elementDescriptionClear = driver.findElement(By.id("createLeadForm_description"));
+		elementDescriptionClear.clear();
+		
+		WebElement elementImportantNote = driver.findElement(By.id("createLeadForm_importantNote"));
+		elementImportantNote.sendKeys("Edited the page");
+		
+		WebElement elementUpdateButton = driver.findElement(By.className("smallSubmit"));
+		elementUpdateButton.click();
+		
+		System.out.println("Title of Resulting Page is :" + driver.getTitle());
+	}
+
+}
